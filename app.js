19339771a2f741f64da9f54288ef5ebb8f934b1d
Tashi Phuntsho <tashitakuley@gmail.com>
2023-12-22 15:53:44 +0600

    Implemented views

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+app.get("/", (req, res) => {
+  const notes = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", notes});
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "about" });
 });
 
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+app.get("/note/create", (req, res) => {
+  res.render("newNote", { title: "New Note" });
 });
 
-app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
-})
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
