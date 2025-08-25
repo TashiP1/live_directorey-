e5b5cffb69e77d4f72c689c16ee9740134ff4425
Taship1 <tashitakuley@gmail.com>
2025-08-25 18:29:00 +0600

    log

--- resetpass.js
+++
@@ -1,8 +0,0 @@
-// reset-password.js
-const bcrypt = require('bcrypt');
-
-const newPassword = "123456";
-bcrypt.hash(newPassword, 10, (err, hash) => {
-  if (err) throw err;
-  console.log("Hashed password:", hash);
-});
